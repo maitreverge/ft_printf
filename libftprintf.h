b44902b3b29dc6b08dc41a7afb4b760be6b2c7ec
flverge_42 <verge.f@gmail.com>
2023-10-19 16:45:12 +0200

    understanding finished

--- libftprintf.h
+++ libftprintf.h
@@ -6,7 +6,7 @@
 /*   By: flverge <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/05 17:22:30 by florianverg       #+#    #+#             */
-/*   Updated: 2023/10/19 09:50:00 by flverge          ###   ########.fr       */
+/*   Updated: 2023/10/19 15:23:05 by flverge          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,35 @@
 # define INT_MAX 2147483647
 # define INT_MIN -2147483648
 
+# define PLACEHOLDERS "%cspdiuxX"
+# define START_FLAGS "# +"
+# define WIDTH_FLAGS "-0"
+# define PRECISION_FLAGS "."
+
 # include <unistd.h>
 # include <stdlib.h>
+
+
+typedef s_struct {
+	int plus_sign;
+	int space;
+	int hashtag;
+} t_start_flags;
+
+
+typedef s_struct {
+	int minus_sign;
+	int zero;
+	unsigned int width;
+} t_width_flags;
+
+
+typedef s_struct {
+	int point;
+	unsigned int precision;
+} t_precision_flags;
+
+
 
 int		ft_printf(const char *format, ...);
 
--- understanding.c
+++ understanding.c
@@ -3,42 +3,139 @@
 int main(void)
 {
 
-	// 3735929054 == deadc0de
-	int i = 1735929054;
-	char str[] = "salut";
+	char c = 'A';
+	char *str = "Salut";
+	char *adress = "qwghtgtrefrg";
+	int pos_int = 123;
+	int neg_int = -42;
+	int hexa = 0xdeadc0de;
+	int neg_hexa = 0xD7;
+	int octal = 0337;
 
-	// printf("%#x\n", i);
-	printf("% s\n", str);
-	// printf("%+ d\n", i);
+
+	// printf("[%c]\n-----\n\n", c);
+
+	printf("[% s]\n-----\n\n", str);
+	
+	// printf("[%p]\n-----\n\n", adress);
+	
+	// printf("%d\n-----\n\n", pos_int);
+	
+	// printf("%d\n-----\n\n", neg_int);
+	
+	// printf("[%.4i]\n-----\n\n", pos_int);
+	
+	// printf("%-12i\n-----\n\n", neg_int);
+
+	// printf("[%-5i]\n-----\n\n", neg_hexa);
+
+// 	printf("%i\n-----\n\n", hexa);
+	
+// 	printf("%u\n-----\n\n", pos_int);
+	
+// 	printf("%x\n-----\n\n", pos_int);
+	
+	// printf("%x\n-----\n\n", neg_int);
+	// printf("%#.10x\n-----\n\n", neg_int);
+	
+// 	printf("%X\n-----\n\n", hexa);
+
+// 	printf("%X\n-----\n\n", neg_hexa);
+	
+	// printf("[%%]\n-----\n\n");
+// }
+
 }
-
 
 /*
 
-GENERAL STRUCTURE OF PRINTF 
+?											GENERAL STRUCTURE OF PRINTF 
 
-% [flags]		[width]			[.precision]  placeholder
+!					% 		[flags]			[width]			[.precision]		placeholder
+
+---------------------------------------------------------------------------------------------------------------
+					! FLAGS :
 
 
-! FLAGS :
-
-- =
+- = Presence de - comme %-5i (avec i = 42) ==> [42   ] %5i => [   42]
 --------------------------------------
-+ = 
++ = Forces the printing of a plus sign for positives int numbers
 --------------------------------------
-' ' (whitespace) = 
+' ' (whitespace) = Add a single space before a positive number
 --------------------------------------
 # = Adds either a 0x (for %x) or a 0X (for %X) 
 --------------------------------------
 0 (zero) = 
 --------------------------------------
 
+* CLASHING FLAGS
+
+
+! ' ' and + => Basically does not works
+
+
+
+
+
+----------------------------------------------------------------------------------------------------------------
+
 ! WIDTH
 
+The concept of widh basically adds up x spaces before or after the number
+
+? FLAGS OF WIDTH ==>
+
+- = Ident the width AFTER 
+Lack of - flag = Ident the width BEFORE
+
+0 = In the first place, ident 0 characters instead of spaces
+
+
+
+----------------------------------------------------------------------------------------------------------------
 ! .PRECISION
 
+.(number)
 
+NUMBER CASES : Cela affiche le nombre de caracteres minimum a afficher,
+Si le chiffre est plus grand que la precision, cela ne tronque pas le chiffre
+
+int n = 42;
+printf("%.1i", n); ==> Outputs 42
+printf("%.2i", n); ==> Outputs 42
+printf("%.3i", n); ==> Outputs 042
+printf("%.4i", n); ==> Outputs 0042
+printf("%.5i", n); ==> Outputs 00042
+
+STRING CASES : Cela affiche le nombre de caractere maximum
+Si la string est plus grande que la precision, dans ce cas la precision prends le dessus
+et tronque la phrase
+
+char *str = "salut";
+
+printf("%s", str); ==> Outputs "salut"
+printf("%.5s", str); ==> Outputs "salut"
+printf("%.4s", str); ==> Outputs "salu"
+printf("%.3s", str); ==> Outputs "sal"
+
+! COMBO CASES
+
+! [] within the printf is to better visualize width filed
+
+char *str = "salut";
+printf("[%-5.4s]\n-----\n\n", str); ==> Outputs [salu ]
+printf("[%5.4s]\n-----\n\n", str); ==> Outputs [ salu]
+
+
+
+printf("%#.10x\n-----\n\n", neg_int); ==> 0x00ffffffd6 so 10 precision and the # flag does not count into the precision
+
+----------------------------------------------------------------------------------------------------------------
 ! PLACEHOLDERS
+
+
+
+
 --------------------------------------
 %c	= Character
 --------------------------------------
@@ -57,8 +154,9 @@ GENERAL STRUCTURE OF PRINTF
 %X	= Unsigned int => Hexa uppercase
 --------------------------------------
 %%	= Print %
---------------------------------------
 
+
+----------------------------------------------------------------------------------------------------------------
 
 
 
