26f584dbed6ff204992cdab7aa3b2b30dadff684
flverge_42 <verge.f@gmail.com>
2023-10-30 10:37:44 +0100

    szdfxgcvhbnerszxdtfcghjb

--- src/maxi_int.c
+++ src/maxi_int.c
@@ -6,7 +6,7 @@
 /*   By: flverge <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/28 10:19:10 by flverge           #+#    #+#             */
-/*   Updated: 2023/10/30 10:35:13 by flverge          ###   ########.fr       */
+/*   Updated: 2023/10/30 10:37:26 by flverge          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -124,7 +124,7 @@ int	preci_1_width_0(long nb, int len_nb, t_flags flags, int sp_pl) // PAS DE GES
 	real_precision = flags.precision - positive_nb(nb);
 
 	sp_pl = print_plus_or_space(nb, flags);
-
+  b 
 	if (nb < 0)
 		ft_putchar('-');
 
