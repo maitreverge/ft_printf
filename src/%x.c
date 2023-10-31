ec1f758b64be9f3184c553490924075f14b9f0e3
flverge_42 <verge.f@gmail.com>
2023-10-31 09:33:19 +0100

    renamed_file

---
+++ src/%x.c
@@ -0,0 +1,177 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   %x.c                                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: flverge <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/30 21:51:32 by flverge           #+#    #+#             */
+/*   Updated: 2023/10/31 09:30:41 by flverge          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+int		print_x(unsigned int n, t_flags flags)
+{
+	unsigned long	nb;
+	int		len_nb;
+	int	hashtag;
+
+	nb = n;
+	len_nb = intlen_hexa(n);
+	hashtag = 0;
+	if (!flags.point)
+		flags.lenght_print = preci_0_x(nb, len_nb, flags, hashtag);
+	else
+		flags.lenght_print = preci_1_x(nb, len_nb, flags, hashtag);
+	return (flags.lenght_print);
+}
+
+int	preci_0_x(unsigned long nb, int len_nb, t_flags flags, int hashtag)
+{
+	if (!flags.width)
+		flags.lenght_print = preci_0_width_0_x(nb, len_nb, flags, hashtag);
+	else
+		flags.lenght_print = preci_0_width_1_x(nb, len_nb, flags, hashtag);
+	return (flags.lenght_print);
+}
+
+int	preci_0_width_0_x(unsigned long nb, int len_nb, t_flags flags, int hashtag)
+{
+	hashtag = print_hashtag(flags);
+	putnbr_hexa(nb, 'x');
+	return (len_nb + hashtag);
+}
+
+int	preci_0_width_1_x(unsigned long nb, int len_nb, t_flags flags, int hashtag)
+{
+	int	real_width;
+
+	if (flags.minus_sign)
+	{
+		hashtag = print_hashtag(flags);
+		putnbr_hexa(nb, 'x');
+		real_width = flags.width - hashtag - len_nb;
+		if (real_width > 0)
+			print_width_space(real_width);
+		else
+			real_width = 0;
+	}
+	else
+	{
+		if (!flags.zero)
+		{
+			real_width = flags.width - determine_hashtag(flags) - len_nb;
+			if (real_width > 0)
+			{
+				if (flags.zero)
+					print_zero(real_width);
+				else
+					print_width_space(real_width);
+			}
+			else
+				real_width = 0;
+			hashtag = print_hashtag(flags);
+			putnbr_hexa(nb, 'x');
+		}
+		else
+		{
+			hashtag = print_hashtag(flags);
+			if (nb < 0)
+				ft_putchar('-');
+			real_width = flags.width - hashtag - len_nb;
+			if (real_width > 0)
+			{
+				if (flags.zero)
+					print_zero(real_width);
+				else
+					print_width_space(real_width);
+			}
+			else
+				real_width = 0;
+			putnbr_hexa(nb, 'x');
+		}
+	}
+	return (len_nb + hashtag + real_width);
+}
+
+
+int	preci_1_x(unsigned long nb, int len_nb, t_flags flags, int hashtag)
+{
+	if (!flags.width)
+		flags.lenght_print = preci_1_width_0(nb, len_nb, flags, hashtag);
+	else
+		flags.lenght_print = preci_1_width_1(nb, len_nb, flags);
+	return (flags.lenght_print);
+}
+
+
+int	preci_1_width_0_x(unsigned long nb, int len_nb, t_flags flags, int hashtag)
+{
+	int	real_precision;
+	int	len_prec;
+
+	len_prec = len_nb;
+	if (nb < 0)
+		len_prec -= 1;
+	if (nb == 0 && flags.precision == 0)
+		return (0);
+	real_precision = flags.precision - len_prec;
+	hashtag = print_hashtag(flags);
+	if (nb < 0)
+		ft_putchar('-');
+	if (real_precision > 0)
+		print_zero(real_precision);
+	else
+		real_precision = 0;
+	putnbr_hexa(nb, 'x');
+	return (len_nb + hashtag + real_precision);
+}
+
+
+int	preci_1_width_1_x(unsigned long nb, int len_nb, t_flags flags)
+{
+	int	real_width;
+	int	real_precision;
+	int	len_prec;
+
+	len_prec = len_nb;
+	if (nb < 0)
+		len_prec -= 1;
+	real_precision = flags.precision - len_prec;
+	if (real_precision < 0)
+		real_precision = 0;
+	real_width = flags.width - determine_hashtag(flags) - len_nb - real_precision;
+	if (real_width < 0)
+		real_width = 0;
+	if (!flags.precision && !nb)
+		return (zero_case_x(flags));
+	if (!flags.minus_sign)
+	{
+		print_width_space(real_width);
+		if (nb < 0)
+			ft_putchar('-');
+		print_hashtag(flags);
+		if (real_precision > 0)
+			print_zero(real_precision);
+		putnbr_hexa(nb, 'x');
+	}
+	else
+	{
+		print_hashtag(flags);
+		if (nb < 0)
+			ft_putchar('-');
+		if (real_precision > 0)
+			print_zero(real_precision);
+		putnbr_hexa(nb, 'x');
+		print_width_space(real_width);
+	}
+	return (len_nb + real_width + real_precision);
+}
+
+int	zero_case_x(t_flags flags)
+{
+	print_width_space(flags.width);
+	return (flags.width);
+}
