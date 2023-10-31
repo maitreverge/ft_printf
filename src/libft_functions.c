edbd88356b15b8ed2b218035ac8b6f841e1b630c
flverge_42 <verge.f@gmail.com>
2023-10-31 12:29:36 +0100

    fusion libft

--- src/libft_functions.c
+++ src/libft_functions.c
@@ -1,16 +1,45 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strlen.c                                        :+:      :+:    :+:   */
+/*   libft_functions.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: flverge <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/02 15:23:43 by flverge           #+#    #+#             */
-/*   Updated: 2023/10/25 12:10:49 by flverge          ###   ########.fr       */
+/*   Created: 2023/10/31 12:16:49 by flverge           #+#    #+#             */
+/*   Updated: 2023/10/31 12:28:51 by flverge          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+
+void	*ft_memset(void *b, int c, size_t len)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < len)
+	{
+		((char *)b)[i] = c;
+		i++;
+	}
+	return (b);
+}
+
+void	ft_bzero(void *s, size_t n)
+{
+	ft_memset(s, 0, n);
+}
+
+void	*ft_calloc(size_t count, size_t size)
+{
+	void	*buffer;
+
+	buffer = malloc(size * count);
+	if (!buffer)
+		return (NULL);
+	ft_bzero(buffer, size * count);
+	return (buffer);
+}
 
 size_t	ft_strlen(const char *s)
 {
@@ -21,6 +50,11 @@ size_t	ft_strlen(const char *s)
 		i++;
 	return (i);
 }
-/*
-Calcule la longeur du buffer *s
-*/
+
+int	ft_isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+	return (0);
+}
+
